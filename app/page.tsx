bd05fa651a2610d2489369cf67942f0362a6ee91
Shehari931 <91692552+Shehari931@users.noreply.github.com>
2024-06-02 03:40:13 +0500

    completing the homepage and add some new feature to navbar and also sorting the folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -63,33 +63,33 @@ export default function Home() {
           <div className="px-4 sm:px-8 lg:px-0 mt-5 sm:mt-10 grid sm:grid-cols-2 justify-center gap-5 sm:gap-10 items-center sm:*:mx-auto">
             <div className="py-2 px-4 border border-primary rounded-[1.125rem] text-primary leading-7  shadow-[0px_0px_8px_0px_rgb(40,54,24,0.34)] max-w-[413px] lg:max-w-[423px] sm:text-center sm:col-start-1 sm:row-start-2 sm:col-span-2">
               <p className="opacity-70" id="one">
-                "I can&apos;t say enough good things about Nativis! Their
+                &quot;I can&apos;t say enough good things about Nativis! Their
                 commitment to using natural ingredients is evident in the
                 quality of their products. Since incorporating Nativis into my
                 skincare routine, I&apos;ve noticed a significant reduction in
                 breakouts and a noticeable improvement in my skin&apos;s overall
                 appearance. Thank you, Nativis, for giving me the confidence to
-                bare my natural beauty!"
+                bare my natural beauty!&quot;
               </p>
               <p className="mt-3 text-center">Emma C. - Toronto, Canada</p>
             </div>
             <div className="py-2 px-4 border border-primary rounded-[1.125rem] text-primary leading-7  shadow-[0px_0px_8px_0px_rgb(40,54,24,0.34)] max-w-[413px] lg:max-w-[423px] sm:text-center">
               <p className="opacity-70">
-                "I&apos;ve tried countless skincare brands over the years, but
-                Nativis truly stands out. Their products are gentle yet
+                &quot;I&apos;ve tried countless skincare brands over the years,
+                but Nativis truly stands out. Their products are gentle yet
                 powerful, and I&apos;ve noticed a remarkable improvement in my
                 skin&apos;s tone and texture. Finally, I&apos;ve found skincare
-                that makes me feel confident and radiant!"
+                that makes me feel confident and radiant!&quot;
               </p>
               <p className="mt-3 text-center">Jenny M. - New York, USA</p>
             </div>
             <div className="py-2 px-4 border border-primary rounded-[1.125rem] text-primary leading-7  shadow-[0px_0px_8px_0px_rgb(40,54,24,0.34)] max-w-[413px] lg:max-w-[423px] sm:text-center">
               <p className="opacity-70">
-                "Nativis has been a game-changer for me! As someone with
+                &quot;Nativis has been a game-changer for me! As someone with
                 sensitive skin, I&apos;ve always struggled to find products that
                 don&apos;t cause irritation. Nativis not only soothes my skin
                 but also leaves it looking healthier and more luminous. I&apos;m
-                beyond impressed!"
+                beyond impressed!&quot;
               </p>
               <p className="mt-3 text-center">Sophie L. - Sydney, Australia</p>
             </div>
